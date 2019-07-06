ffbf234168ae2e43378ee9b08bdd13120276a929
bkane <bbkanego@gmail.com>
2019-07-05 21:41:04 -0400

    Updated scripts to create instances and ELB on AWS

---
+++ docs/AWS/scripts/deployUICode.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo 'Build UI code'
+
+cd ~/svn/code/angular/SeerlogicsBotAdminUI/main-app
+npm run prodBuild
+
+echo 'copy angular code to S3'
+
+#https://qiita.com/alokrawat050/items/56820afdb6968deec6a2
+#aws s3api delete-objects --bucket seerbotadmin.seersense.com --delete file://delete.json
+aws s3 rm s3://seerbotadmin.seersense.com --recursive --profile bizBotAdmin
+aws s3 cp /Users/bkane/svn/code/angular/SeerlogicsBotAdminUI/main-app/dist s3://seerbotadmin.seersense.com --profile bizBotAdmin --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers --recursive
