f509ee8fd1acbae7cf539fcbd341000dcfa0b453
bkane <kanebhushan@gmail.com>
2019-12-23 12:06:26 -0500

    Git push/pull

---
+++ scripts/git-pull.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+cd ~/svn/code/java/SeerlogicsBom
+git pull origin develop
+
+cd ~/svn/code/java/SpringMVC/eg-commons
+git pull origin eventGenie-develop
+
+cd ~/svn/code/java/SpringMVC/eg-spring
+git pull origin eventGenie-develop
+
+cd ~/svn/code/java/SpringMVC/eg-cloud-support
+git pull origin eventGenie-develop
+
+cd ~/svn/code/java/SeerlogicsCloud
+git pull origin develop
+
+cd ~/svn/code/java/SeerlogicsCommons
+git pull origin develop
+
+cd ~/svn/code/java/SeerlogicsBotAdmin
+git pull origin develop
+
+cd ~/svn/code/java/SeerLogicsReferenceBot
+git pull origin develop
+
+cd ~/svn/code/angular/Angular-Library-With-NgPackagr
+git pull origin develop
+
+cd ~/svn/code/angular/SeerlogicsBotAdminUI
+git pull origin develop
+
+cd ~/svn/code/angular/SeerlogicsChatClientUI
+git pull origin develop
---
+++ scripts/git-push.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+cd ~/svn/code/java/SeerlogicsBom
+git push origin develop
+
+cd ~/svn/code/java/SpringMVC/eg-commons
+git push origin eventGenie-develop
+
+cd ~/svn/code/java/SpringMVC/eg-spring
+git push origin eventGenie-develop
+
+cd ~/svn/code/java/SpringMVC/eg-cloud-support
+git push origin eventGenie-develop
+
+cd ~/svn/code/java/SeerlogicsCloud
+git push origin develop
+
+cd ~/svn/code/java/SeerlogicsCommons
+git push origin develop
+
+cd ~/svn/code/java/SeerlogicsBotAdmin
+git push origin develop
+
+cd ~/svn/code/java/SeerLogicsReferenceBot
+git push origin develop
+
+cd ~/svn/code/angular/Angular-Library-With-NgPackagr
+git push origin develop
+
+cd ~/svn/code/angular/SeerlogicsBotAdminUI
+git push origin develop
+
+cd ~/svn/code/angular/SeerlogicsChatClientUI
+git push origin develop
