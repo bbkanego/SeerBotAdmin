55eecd7c09261a2a74ba5b62aaf0c30ab2af2573
Bhushan Kane <bbkanego@gmail.com>
2020-02-14 12:47:51 -0500

    Build and clone scripts

---
+++ scripts/git-clone.bat
@@ -0,0 +1,9 @@
+
+cd C:\Bhushan\code\java
+
+git clone -b develop http://bubuntu:3000/bkane/SeerlogicsBotCommons
+git clone -b develop http://bubuntu:3000/bkane/SeerlogicsBom
+git clone -b develop http://bubuntu:3000/bkane/SeerLogicsSharedBot
+git clone -b develop http://bubuntu:3000/bkane/SeerLogicsBotStateMachine
+git clone -b develop http://bubuntu:3000/bkane/SeerlogicsCloud
+git clone -b eventGenie-develop http://bubuntu:3000/bkane/EventGenie-Angular-Spring
