8619361a14363ffb20498d908ad4311e7215bfe9
Bhushan Kane <bbkanego@gmail.com>
2020-02-16 13:38:17 -0500

    Changed to make things work in UBUNTU

--- scripts/build-all.sh
+++ scripts/build-all.sh
@@ -9,11 +9,13 @@ mvn -f $JAVA_CODE_PATH/SeerlogicsBom/pom.xml  clean install
 
 mvn -f $JAVA_CODE_PATH/EventGenie-Angular-Spring/eg-commons/pom.xml  clean install
 
-mvn -f $JAVA_CODE_PATHEventGenie-Angular-Spring/eg-spring/pom.xml  clean install
+mvn -f $JAVA_CODE_PATH/EventGenie-Angular-Spring/eg-spring/pom.xml  clean install
 
 mvn -f $JAVA_CODE_PATH/SeerlogicsCloud/pom.xml  clean install
 
 mvn -f $JAVA_CODE_PATH/SeerlogicsBotCommons/pom.xml  clean install
+
+mvn -f $JAVA_CODE_PATH/SeerLogicsBotStateMachine/pom.xml  clean install
 
 mvn -f $JAVA_CODE_PATH/SeerlogicsBotAdmin/pom.xml  clean install
 
