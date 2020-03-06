735c86271b27f81224586cb04b4356eda6ead7ed
Bhushan Kane <bbkanego@gmail.com>
2020-03-05 20:21:17 -0500

    Create a popout component and make Help use that

--- docs/AWS/scripts/deployUICode.sh
+++ docs/AWS/scripts/deployUICode.sh
@@ -2,7 +2,7 @@
 
 echo 'Build UI code'
 
-cd ~/svn/code/angular/SeerlogicsBotAdminUI/main-app
+cd $code_home/angular/SeerlogicsBotAdminUI/main-app
 npm run prodBuild
 
 echo 'copy angular code to S3'
@@ -10,4 +10,4 @@ echo 'copy angular code to S3'
 #https://qiita.com/alokrawat050/items/56820afdb6968deec6a2
 #aws s3api delete-objects --bucket gab.seersense.com --delete file://delete.json
 aws s3 rm s3://gab.seersense.com --recursive --profile bizBotAdmin
-aws s3 cp /Users/bkane/svn/code/angular/SeerlogicsBotAdminUI/main-app/dist s3://gab.seersense.com --profile bizBotAdmin --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers --recursive
+aws s3 cp $code_home/angular/SeerlogicsBotAdminUI/main-app/dist s3://gab.seersense.com --profile bizBotAdmin --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers --recursive
--- docs/AWS/scripts/launchBotAdmin.sh
+++ docs/AWS/scripts/launchBotAdmin.sh
@@ -9,34 +9,34 @@
 s3commonKeyPrefix='bkane/BotAdminApp/'
 
 botAdminArtifact='seerlogics-bot-admin-1.0.0-SNAPSHOT.war'
-botAdminBlob='~/svn/code/java/SeerlogicsBotAdmin/target/'$botAdminArtifact
+botAdminBlob=$code_home'/java/SeerlogicsBotAdmin/target/'$botAdminArtifact
 s3botAdminDataKey=$s3commonKeyPrefix$botAdminArtifact
 
 serverXMLArtifact='server.xml'
-serverXMLBlob='/Users/bkane/svn/code/java/SeerlogicsBotAdmin/tomcat/AWS/'$serverXMLArtifact
+serverXMLBlob=$code_home'/java/SeerlogicsBotAdmin/tomcat/AWS/'$serverXMLArtifact
 s3serverXMLDataKey=$s3commonKeyPrefix$serverXMLArtifact
 
 botDBArtifact='botDB.mv.db'
-botDBBlob='~/svn/code/java/SeerlogicsBotAdmin/h2/'$botDBArtifact
+botDBBlob=$code_home'/java/SeerlogicsBotAdmin/h2/'$botDBArtifact
 s3botDBDataKey=$s3commonKeyPrefix$botDBArtifact
 
 chatBotDbArtifact='chatBotServerDB.mv.db'
-chatBotDbBlob='~/svn/code/java/SeerlogicsBotAdmin/h2/'$chatBotDbArtifact
+chatBotDbBlob=$code_home'/java/SeerlogicsBotAdmin/h2/'$chatBotDbArtifact
 s3chatBotDBDataKey=$s3commonKeyPrefix$chatBotDbArtifact
 
 bucket='biz-bot-artifact'
 
-~/svn/code/java/SeerlogicsBotAdmin/docs/AWS/scripts/terminateEC2Instances.sh
+$code_home/java/SeerlogicsBotAdmin/docs/AWS/scripts/terminateEC2Instances.sh
 
 echo 'build BotAdmin artifact now'
-cd ~/svn/code/java/SeerlogicsBotAdmin
+cd $code_home/java/SeerlogicsBotAdmin
 mvn clean install -P aws-ec2-war
 
 echo 'Build done ------------------'
 
-cd ~/svn/code/java/SeerlogicsBotAdmin/docs/AWS/scripts
+cd $code_home/java/SeerlogicsBotAdmin/docs/AWS/scripts
 
-# cd to ~/Users/bkane/svn/code/java/SeerlogicsBotAdmin/target
+# cd to ~$code_home/java/SeerlogicsBotAdmin/target
 echo 'Copying deployable artifacts to S3: ' $botAdminBlob $botDBBlob $chatBotDbBlob $serverXMLBlob
 aws s3api put-object --profile bizBotAdmin --body $botAdminBlob --bucket $bucket --key $s3botAdminDataKey
 aws s3api put-object --profile bizBotAdmin --body $botDBBlob --bucket $bucket --key $s3botDBDataKey
@@ -44,6 +44,7 @@ aws s3api put-object --profile bizBotAdmin --body $chatBotDbBlob --bucket $bucke
 aws s3api put-object --profile bizBotAdmin --body $serverXMLBlob --bucket $bucket --key $s3serverXMLDataKey
 
 echo 'Launching instances now'
+# AWS Amazon linux image id: ami-0998bf58313ab53da
 #aws ec2 run-instances --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=BizBotAdmin}]' --region us-east-2 --profile bizBotAdmin --image-id ami-0cd3dfa4e37921605 --key-name bizBotAdminLogin --security-groups bizBotSecurityGroup --instance-type t2.micro --placement AvailabilityZone=us-east-2c --block-device-mappings DeviceName=/dev/sdh,Ebs={VolumeSize=100} --count 1
 aws ec2 run-instances --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=BizBotAdmin}]' --user-data file://onLaunchScript.sh --region us-east-2 --iam-instance-profile Name="S3_biz_bot_artifact_Readonly" --image-id ami-0cd3dfa4e37921605 --key-name bizBotAdminLogin --security-groups bizBotSecurityGroup --instance-type t2.micro --placement AvailabilityZone=us-east-2c --count 2
 
