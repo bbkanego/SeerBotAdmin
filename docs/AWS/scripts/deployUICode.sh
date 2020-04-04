6f047a6c6fd057811b2e834f63431d4ff88f4704
bkane <bbkanego@gmail.com>
2020-04-04 19:48:48 -0400

    Fix small issues, improve help, fix issues with error handling

--- docs/AWS/scripts/deployUICode.sh
+++ docs/AWS/scripts/deployUICode.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+botAdminUI=$code_home/angular/SeerlogicsBotAdminUI
+
+echo 'Building bootstrap chat client JS'
+cd $botAdminUI/chat-client-bootstrap
+npm run prepack
+cp $botAdminUI/chat-client-bootstrap/public/javascript/seer-chat-bootstrap.min.js $botAdminUI/main-app/src/assets/bootstrap/seer-chat-bootstrap.min.js
+
 echo 'Build UI code'
 
-cd $code_home/angular/SeerlogicsBotAdminUI/main-app
+cd $botAdminUI/main-app
 npm run prodBuild
 
 echo 'copy angular code to S3'
@@ -10,4 +17,4 @@ echo 'copy angular code to S3'
 #https://qiita.com/alokrawat050/items/56820afdb6968deec6a2
 #aws s3api delete-objects --bucket gab.seersense.com --delete file://delete.json
 aws s3 rm s3://gabadmin.seersense.com --recursive --profile bizBotAdmin
-aws s3 cp $code_home/angular/SeerlogicsBotAdminUI/main-app/dist s3://gabadmin.seersense.com --profile bizBotAdmin --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers --recursive
+aws s3 cp $botAdminUI/main-app/dist s3://gabadmin.seersense.com --profile bizBotAdmin --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers --recursive
