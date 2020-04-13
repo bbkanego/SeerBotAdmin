4ab1cd72f9abfea231249f5f6cb97351dd905acc
bkane <bbkanego@gmail.com>
2020-04-13 14:55:46 -0400

    Fix small issues, improve help, fix issues with error handling

--- docs/AWS/scripts/deployUICode.sh
+++ docs/AWS/scripts/deployUICode.sh
@@ -16,5 +16,5 @@ echo 'copy angular code to S3'
 
 #https://qiita.com/alokrawat050/items/56820afdb6968deec6a2
 #aws s3api delete-objects --bucket gab.seersense.com --delete file://delete.json
-aws s3 rm s3://gabadmin.seersense.com --recursive --profile bizBotAdmin
-aws s3 cp $botAdminUI/main-app/dist s3://gabadmin.seersense.com --profile bizBotAdmin --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers --recursive
+aws s3 rm s3://gabstatic.seersense.com --recursive --profile bizBotAdmin
+aws s3 cp $botAdminUI/main-app/dist s3://gabstatic.seersense.com --profile bizBotAdmin --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers --recursive
